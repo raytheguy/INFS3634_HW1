026787a9e29fb29a93f44501255d6feaebefeb25
raytheguy <55873005+raytheguy@users.noreply.github.com>
2019-10-09 17:56:33 +1100

    File System Sync
    Synced with file system

--- ScrollingApplicationP2/app/src/main/java/com/example/scrollingapplication/RecyclerViewAdapter.java
+++ ScrollingApplicationP2/app/src/main/java/com/example/scrollingapplication/RecyclerViewAdapter.java
@@ -2,7 +2,6 @@ package com.example.scrollingapplication;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
